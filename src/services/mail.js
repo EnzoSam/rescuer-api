9c43dd6132ca6378e11dfaeb00645e23e9324231
enzo256 <enzosamudio@gmail.com>
2023-02-26 16:17:04 -0300

    pasaje de datos al .env

--- src/services/mail.js
+++ src/services/mail.js
@@ -3,7 +3,7 @@ var nodemailer = require('nodemailer');
 //var smtpTransport = require('nodemailer-smtp-transport');
 
 //const webHost = 'http://localhost:4200';
-const webHost = 'https://rescatistas.000webhostapp.com/#';
+const webHost = process.env.VALIDATION_ACCOUNT_URL;
 
 exports.enviarConfirmacion = async function (destinatario, nombreDestinatario, codigoConfirmacion) {
 
@@ -11,13 +11,13 @@ exports.enviarConfirmacion = async function (destinatario, nombreDestinatario, c
     //var smtpTransport = require('nodemailer-smtp-transport');
 
     var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false, // true for 465, false for other ports
+      service: process.env.SMTP_SERVICE,
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: process.env.SMTP_SECURE, // true for 465, false for other ports
       auth: {
-        user: 'same.devs.ok@gmail.com',
-        pass: 'epsilonsigmayox23'
+        user: process.env.SMTP_MAIL,
+        pass: process.env.SMTP_PASSWORD
       }
     });
 
@@ -38,13 +38,13 @@ function ObtenerMailOptionActivacion(nombreDestinatario, destinatario,codigoConf
 {
   let codigoValidacion = '<p><h2>Hola ' + nombreDestinatario +
   '</h2></p><p><h3>Este es tu codigo de validación</h3></p><p><h1>' + codigoConfirmacion + '</h1></>';
-  let hr = webHost + '/usuario/validar-codigo/'+ destinatario + '/'+ codigoConfirmacion;
+  let hr = webHost +  destinatario + '/'+ codigoConfirmacion;
   let linkValidacion = '<p><h2>Hola ' + nombreDestinatario +
   '</h2></p><p><h4>Haz click en el siguiente enlace para activar tu cuenta</h4></p><p><a href="' + hr + '">' + hr+ '</a>';
 //http://localhost:3999/api/usuario/validarcodigo/samudioenzo@yahoo.com/codigo/12123
   console.log(linkValidacion);
   let mailOptions = {
-    from: 'same.devs.ok@gmail.com',
+    from: process.env.SMTP_MAIL,
     to: destinatario,
     subject: 'Activación de cuenta',
     text: '',
@@ -59,13 +59,13 @@ function ObtenerMailOptionResetPasword(nombreDestinatario, destinatario,codigoCo
 {
   let codigoValidacion = '<p><h2>Hola ' + nombreDestinatario +
   '</h2></p><p><h3>Este es tu codigo de validación</h3></p><p><h1>' + codigoConfirmacion + '</h1></>';
-  let hr =  webHost + '/usuario/reset-password/'+ destinatario + '/codigo/'+ codigoConfirmacion;
+  let hr =  webHost + destinatario + '/codigo/'+ codigoConfirmacion;
   let linkValidacion = '<p><h2>Hola ' + nombreDestinatario +
   '</h2></p><p><h4>Haz click en el siguiente enlace para activar tu cuenta</h4></p><p><a href="' + hr + '">' + hr+ '</a>';
 //http://localhost:4200/usuario/validarcodigo/samudioenzo@yahoo.com/codigo/12123
   console.log(linkValidacion);
   let mailOptions = {
-    from: 'same.devs.ok@gmail.com',
+    from: process.env.SMTP_MAIL,
     to: destinatario,
     subject: 'Activación de cuenta',
     text: '',
